2fb38402680b2415ea86d0e05db0a16f03dd0bbe
João Pedro Alves da Silva Gomes <joao.pedro.alves.sg@gmail.com>
2025-03-20 16:07:17 -0300

    att: pacienteRoutes

--- src/routes/pacienteRoutes.js
+++ src/routes/pacienteRoutes.js
@@ -3,9 +3,9 @@ const Paciente = require("../controllers/pacienteController");
 const router = express.Router();
 
 router.get("/", Paciente.listarPacientes);
-router.get("/:id", Paciente.buscarPacientesPorId);
+router.get("/:id", Paciente.buscarPacientePorId);
 router.post("/", Paciente.criarPacientes);
 router.put("/:id", Paciente.atualizarPacientes);
-router.delete("/:id", Paciente.excluirPacientes);
+router.delete("/:id", Paciente.excluirPaciente);
 
 module.exports = router;
