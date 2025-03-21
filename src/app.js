d67d4e9d52fdf317b657d1299b64ea4fa3648aff
Ingryd <ingrydcduarte@gmail.com>
2025-03-21 09:44:54 -0300

    wip: userRoutes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const pacienteRoutes = require("./routes/pacienteRoutes");
+const userRoutes = require("./routes/userRoutes")
+
 app.use(express.json());
 app.use("/paciente", pacienteRoutes);
+app.use("/users", userRoutes)
 
 module.exports = app;
