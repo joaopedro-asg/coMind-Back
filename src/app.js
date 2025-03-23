cf56c4a71bd08d089305d2554a97a424e4419b90
Ingryd <ingrydcduarte@gmail.com>
2025-03-23 14:28:51 -0300

    setup!: cors and users/auth routes to app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const userRoutes = require('./routes/userRoutes');
-//const pacienteRoutes = require('./routes/pacienteRoutes');
+import express, { json } from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes';
+import userRoutes from './routes/userRoutes';
+// import pacienteRoutes from './routes/pacienteRoutes';
 
 const app = express();
 
-app.use(express.json());
-app.use('/usuarios', userRoutes);
-//app.use('/pacientes', pacienteRoutes);
+app.use(json());
+app.use(cors());
 
-module.exports = app;
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+// app.use('/api/pacientes', pacienteRoutes);
+
+export default app;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
-const app = require('./app');
-const PORT = process.env.PORT || 3001;
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
