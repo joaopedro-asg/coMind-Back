8169c3bdb155e05f066840ca25d3d6c776699963
Ingryd <ingrydcduarte@gmail.com>
2025-03-23 14:30:56 -0300

    feat: authroutes and useroutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,26 @@
 import express from "express";
-import { register, login } from "../controllers/authController";
+import { registrarUsuario, autenticarUsuario } from "../auth/authService.js";
 
 const router = express.Router();
 
-router.post("auth/register", register);
-router.post("auth/login", login);
+router.post("/register", async (req, res) => {
+  try {
+    const { nomeUsario, email, senha, tipo } = req.body;
+    const novoUsuario = await registrarUsuario(nomeUsario, email, senha, tipo);
+    res.status(201).json(novoUsuario);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
-export default authRoutes;
+router.post("/login", async (req, res) => {
+  try {
+    const { email, senha } = req.body;
+    const token = await autenticarUsuario(email, senha);
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+export default router;
