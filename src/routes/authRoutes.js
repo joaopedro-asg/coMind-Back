cb3a146236c66f61df0b76b38654acecb0515a0f
Ingryd <ingrydcduarte@gmail.com>
2025-03-26 13:51:23 -0300

    fix: clean authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,24 +3,7 @@ import { registrarUsuario, autenticarUsuario } from "../auth/authService.js";
 
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  try {
-    const { nomeUsario, email, senha, tipo } = req.body;
-    const novoUsuario = await registrarUsuario(nomeUsario, email, senha, tipo);
-    res.status(201).json(novoUsuario);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  try {
-    const { email, senha } = req.body;
-    const token = await autenticarUsuario(email, senha);
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/register", registrarUsuario);
+router.post("/login", autenticarUsuario);
 
 export default router;
