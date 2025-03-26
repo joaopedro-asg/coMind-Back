5398dc12a46035a47b410adab2472cc554a3db17
Ingryd <ingrydcduarte@gmail.com>
2025-03-26 13:50:46 -0300

    fix: app and server js (redundance)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
-import express, { json } from 'express';
 import cors from 'cors';
-import authRoutes from './routes/authRoutes';
-import userRoutes from './routes/userRoutes';
+import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 // import pacienteRoutes from './routes/pacienteRoutes';
 
 const app = express();
-
-app.use(json());
 app.use(cors());
+app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 // app.use('/api/pacientes', pacienteRoutes);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,4 @@
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
