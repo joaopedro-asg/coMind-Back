cc75e19b89e390fa34346e73752407a25047be86
Stephany Oliveira da Silva <stephany16ce@outlook.com>
2025-03-26 18:13:15 -0300

    crud-profissional

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const pacienteRoutes = require("./routes/pacienteRoutes");
 const evolucaoClinicaRoutes = require("./routes/evolucaoClinicaRoutes");
+const profissionalRouter = require("./routes/profissionalRouter");
 
 app.use(express.json());
 app.use("/paciente", pacienteRoutes);
 app.use("/evolucaoClinica", evolucaoClinicaRoutes);
+app.use("/profissionais", profissionalRouter);
 
 module.exports = app;
--- src/controllers/pacienteController.js
+++ src/controllers/pacienteController.js
@@ -1,1 +1,2 @@
 const paciente = require("../models/pacienteModel")
+
