c82e381dbd21a552b544a09968de9a83084ac2ab
João Pedro Alves da Silva Gomes <joao.pedro.alves.sg@gmail.com>
2025-03-26 23:02:14 -0300

    att: app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const express = require("express");
 const app = express();
 const pacienteRoutes = require("./routes/pacienteRoutes");
 const depoimentosRoutes = require("./routes/depoimentosRoutes")
+const gruposApoioRoutes = require("./routes/gruposApoioRoutes")
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +15,7 @@ app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use("/paciente", pacienteRoutes);
 app.use("/depoimentos", depoimentosRoutes);
+app.use("/depoimentos", gruposApoioRoutes);
 
 
 export default app;
