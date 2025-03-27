7c893eaf9954b3b90ec8339ac1ee5f93192f4832
Ingryd <ingrydcduarte@gmail.com>
2025-03-27 15:27:53 -0300

    feat: app use profileRoutes and better router to users

--- src/app.js
+++ src/app.js
@@ -3,15 +3,17 @@ import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import pacienteRoutes from './routes/pacienteRoutes.js';
-import evolucaoClinicaRoutes from './routes/evolucaoClinicaRoute.js';
+import evolucaoClinicaRoutes from './routes/evolucaoClinicaRoutes.js';
+import profileRoutes from './routes/profileRoutes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
+app.use('/api/users', userRoutes);
 app.use("/paciente", pacienteRoutes);
 app.use("/evolucaoClinica", evolucaoClinicaRoutes);
+app.use("/perfil", profileRoutes)
 
 export default app;
