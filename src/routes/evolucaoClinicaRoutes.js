7750110de68bdf671e852db6d2fbae85382df4d4
Ingryd <ingrydcduarte@gmail.com>
2025-03-27 15:57:51 -0300

    fix: evolucaoClinica (somente autenticado)

--- src/routes/evolucaoClinicaRoutes.js
+++ src/routes/evolucaoClinicaRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import * as evolucaoController from '../controllers/evolucaoClinicaController.js';
+import * as authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/evolucaoClinica', evolucaoController.criarEvolucaoClinica);
-router.get('/evolucaoClinica', evolucaoController.getEvolucaoClinica);
-router.get('/evolucaoClinica/:id', evolucaoController.getEvolucaoClinicaById);
-router.put('/evolucaoClinica/:id', evolucaoController.updateEvolucaoClinica);
-router.delete('/evolucaoClinica/:id', evolucaoController.excluirEvolucaoClinica);
+router.use(authMiddleware.authenticate);
+
+router.post('/', authMiddleware.authenticate, evolucaoController.criarEvolucaoClinica);
+router.get('/', authMiddleware.authenticate, evolucaoController.getEvolucaoClinica);
+router.get('/evolucaoClinica/:id', authMiddleware.verificarProfissional, evolucaoController.getEvolucaoClinicaById);
+router.put('/evolucaoClinica/:id', authMiddleware.authenticate, evolucaoController.updateEvolucaoClinica);
+router.delete('/evolucaoClinica/:id',authMiddleware.authenticate, evolucaoController.excluirEvolucaoClinica);
 
 export default router;
