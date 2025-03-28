31dda14c7f6cfe23cb2235d2a44e25de7e1cf319
Ingryd <ingrydcduarte@gmail.com>
2025-03-28 17:54:34 -0300

    update: criarPaciente

--- src/models/pacienteModel.js
+++ src/models/pacienteModel.js
@@ -1,60 +1,95 @@
-import prisma from '../prisma.js'
+import prisma from "../prisma.js";
 
 export const listarPacientes = async () => {
-    return await prisma.paciente.findMany();
+  return await prisma.paciente.findMany();
 };
 
 export const buscarPacientesPorId = async (id) => {
-    const paciente = await prisma.paciente.findUnique({
-        where: { id }
-    });
-    if (!paciente) {
-        throw new Error('Paciente não encontrado...');
-    };
-    return paciente;
+  const paciente = await prisma.paciente.findUnique({
+    where: { usuarioID: Number(id) },
+    include: {
+      usuario: {
+        select: {
+          nomeUsario: true,
+          email: true,
+          tipo: true,
+        },
+      },
+    },
+  });
+
+  if (!paciente) {
+    return res.status(404).json({ error: "Paciente não encontrado" });
+  }
+  return paciente;
 };
 
-export const criarPacientes = async (usuarioID, nomeCompleto, genero, idade, principaisQueixas, usoDeMedicamentos, objetivoDaTerapia, historicoFamiliar, atendimentos, evolucoesClinicas, depoimentos) => {
+export const criarPacientes = async (data) => {
     return await prisma.paciente.create({
-        data:
-        {
-            usuarioID,
-            nomeCompleto, 
-            genero, 
-            idade, 
-            principaisQueixas, 
-            usoDeMedicamentos, 
-            objetivoDaTerapia, 
-            historicoFamiliar,
-            atendimentos,
-            evolucoesClinicas,
-            depoimentos 
-        }
+      data: {
+        usuarioID: data.usuarioID,
+        nomeCompleto: data.nomeCompleto,
+        genero: data.genero,
+        idade: data.idade,
+        principaisQueixas: data.principaisQueixas,
+        usoDeMedicamentos: data.usoDeMedicamentos,
+        objetivoDaTerapia: data.objetivoDaTerapia,
+        historicoFamiliar: data.historicoFamiliar,
+        atendimentos: { create: [] },
+        evolucoesClinicas: { create: [] },
+      }
     });
-};
+  };
 
-export const atualizarPacientes = async (id, { usuarioID, nomeCompleto, genero, idade, principaisQueixas, usoDeMedicamentos, objetivoDaTerapia, historicoFamiliar, atendimentos, evolucoesClinicas, depoimentos }) => {
-    const paciente = await prisma.paciente.findUnique({
-        where: { id }
-    });
-    if (!paciente) {
-        throw new Error('Paciente não atualizado...');
-    };
-    return await prisma.paciente.update({
-        where: { id },
-        data: { usuarioID, nomeCompleto, genero, idade, principaisQueixas, usoDeMedicamentos, objetivoDaTerapia, historicoFamiliar, atendimentos, evolucoesClinicas, depoimentos }
-    });
+export const atualizarPacientes = async (
+  id,
+  {
+    usuarioID,
+    nomeCompleto,
+    genero,
+    idade,
+    principaisQueixas,
+    usoDeMedicamentos,
+    objetivoDaTerapia,
+    historicoFamiliar,
+    atendimentos,
+    evolucoesClinicas,
+    depoimentos,
+  }
+) => {
+  const paciente = await prisma.paciente.findUnique({
+    where: { id },
+  });
+  if (!paciente) {
+    throw new Error("Paciente não atualizado...");
+  }
+  return await prisma.paciente.update({
+    where: { id },
+    data: {
+      usuarioID,
+      nomeCompleto,
+      genero,
+      idade,
+      principaisQueixas,
+      usoDeMedicamentos,
+      objetivoDaTerapia,
+      historicoFamiliar,
+      atendimentos,
+      evolucoesClinicas,
+      depoimentos,
+    },
+  });
 };
 
 export const excluirPacientes = async (id) => {
-    const paciente = await prisma.paciente.findUnique({
-        where: { id }
-    });
-    if (!paciente) {
-        throw new Error('Paciente não excluído...');
-    };
+  const paciente = await prisma.paciente.findUnique({
+    where: { id },
+  });
+  if (!paciente) {
+    throw new Error("Paciente não excluído...");
+  }
 
-    await prisma.paciente.delete({
-        where: { id }
-    });
+  await prisma.paciente.delete({
+    where: { id },
+  });
 };
